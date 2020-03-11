a9b677dbf14d231092d28d220ecc395597baeea4
Malte Wegener <malte.wegener99@gmx.de>
2020-03-11 09:49:51 +0100

    added aoa and q

--- phugoid.m
+++ phugoid.m
@@ -9,13 +9,21 @@ t = t(t_start:t_end)-t_start/10;
 V_0 = flightdata.Dadc1_tas.data(t_start);
 alpha_0 = flightdata.vane_AOA.data(t_start);
 elev_0 = flightdata.delta_e.data(t_start);
+q_0 = flightdata.Ahrs1_bPitchRate.data(t_start);
 hp = flightdata.Dadc1_alt.data(t_start);
+
 
 input = flightdata.delta_e.data(t_start:t_end)-elev_0;
 input = input/180*pi;
 tas = flightdata.Dadc1_tas.data(t_start:t_end)-V_0;
+aoa = flightdata.vane_AOA.data(t_start:t_end)-alpha_0;
+q = flightdata.Ahrs1_bPitchRate.data(t_start:t_end)-q_0;
 
 
 y = lsim(symmetric, input, t);
 plot(t, y(:,1), t, tas);
 legend("Simulation", "Real flight");
+plot(t, y(:,2), t, aoa);
+legend("Simulation", "Real flight");
+% plot(t, y(:,4), t, q);
+% legend("Simulation", "Real flight");
