f54ca978c559748c2f76317b9acab5cf95cc32d6
Malte Wegener <malte.wegener99@gmx.de>
2020-03-12 13:47:41 +0100

    filtering didnt really solve anythiong

--- phugoid.m
+++ phugoid.m
@@ -2,11 +2,10 @@ run("statespace_symmetric.m");
 load("matlab.mat");
 
 
-flightdata.vane_AOA.data = lowpass(flightdata.vane_AOA.data,0.02,10);
-
+aoa = flightdata.vane_AOA.data;
 t = flightdata.time.data;
 sampling_rate = 1/(t(2)-t(1));
-t_start = 31840;
+t_start = 31854;
 t_end = 34450;
 t = t(t_start:t_end)-t(t_start);
 
@@ -14,7 +13,7 @@ hp = flightdata.Dadc1_alt.data(t_start)
 
 
 V_0 = flightdata.Dadc1_cas.data(t_start)
-alpha_0 = flightdata.vane_AOA.data(t_start);
+alpha_0 = aoa(t_start);
 elev_0 = flightdata.delta_e.data(t_start);
 q_0 = flightdata.Ahrs1_bPitchRate.data(t_start);
 th_0 = flightdata.Ahrs1_Pitch.data(t_start);
@@ -23,7 +22,7 @@ th_0 = flightdata.Ahrs1_Pitch.data(t_start);
 input = flightdata.delta_e.data(t_start:t_end)-elev_0;
 input = input/180*pi;
 tas = flightdata.Dadc1_cas.data(t_start:t_end)-V_0;
-aoa = flightdata.vane_AOA.data(t_start:t_end)-alpha_0;
+aoa =aoa(t_start:t_end)-alpha_0;
 q = flightdata.Ahrs1_bPitchRate.data(t_start:t_end)-q_0;
 th = flightdata.Ahrs1_Pitch.data(t_start:t_end)-th_0;
 
